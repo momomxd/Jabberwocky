57ab43a3107b8799a95fa8c9f1dc97ae5a2f329b
moham <moham@172.28.133.190>
2022-10-07 15:32:40 +0200

    MVC Klasse bearbeitet

--- src/jabberwocky_maxamed_yilmaz/Jabberwocky_MVC.java
+++ src/jabberwocky_maxamed_yilmaz/Jabberwocky_MVC.java
@@ -1,9 +1,22 @@
 package jabberwocky_maxamed_yilmaz;
 
-public class Jabberwocky_MVC {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Jabberwocky_MVC extends Application{
+	
+	private Jabberwocky_Model model;
+	private Jabberwocky_View view;
+
+	
+	public void start(Stage primaryStage) throws Exception {
+		model = new Jabberwocky_Model();
+		view = new Jabberwocky_View(model, view);
+		view.start();
+	}
+	public static void main(String[] args)  {
+
+		launch();
 
 	}
 
