159c8655251339abab4110a2e3aa9e79c6c72f2b
moham <moham@DESKTOP-OGH83Q8.edu.ds.fhnw.ch>
2022-10-07 11:45:22 +0200

    - Loop um über text zu gehen (Idee)

--- src/jabberwocky_maxamed_yilmaz/Jabberwocky_Model.java
+++ src/jabberwocky_maxamed_yilmaz/Jabberwocky_Model.java
@@ -1,5 +1,24 @@
 package jabberwocky_maxamed_yilmaz;
 
 public class Jabberwocky_Model {
+	
+	public static void main(String[] args) {
+		
+	
+		String text ="Für den Aufbau der Statistik verwenden wir ein bewegliches Fenster über die Zeichen des Eingabetexts.";
+	
+	
+	char  char1 = text.charAt(2);
+	System.out.println(char1);
+	String [] textparts = {};
+	
+	for (int i = 0; i<text.length()-1; i++) {								//Loop um über den ganzen text zu gehen
+	textparts [i] = text.substring(text.charAt(i), text.charAt(i+1));		//Substring methode speichert die Fenstergrösse von 2 in einem String Array
 
-}
+	for(String s : textparts) {												//Ausgabe des Textes
+		System.out.println(s);
+
+	}
+	
+
+}}}
