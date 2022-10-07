0c24a227a3c6df77f14248253b1845f1d922ca09
moham <moham@172.28.133.190>
2022-10-07 15:41:50 +0200

    View deklaration angepasst

--- src/jabberwocky_maxamed_yilmaz/Jabberwocky_MVC.java
+++ src/jabberwocky_maxamed_yilmaz/Jabberwocky_MVC.java
@@ -8,10 +8,10 @@ public class Jabberwocky_MVC extends Application{
 	private Jabberwocky_Model model;
 	private Jabberwocky_View view;
 
-	
+	@Override
 	public void start(Stage primaryStage) throws Exception {
 		model = new Jabberwocky_Model();
-		view = new Jabberwocky_View(model, view);
+		view = new Jabberwocky_View(primaryStage, model);
 		view.start();
 	}
 	public static void main(String[] args)  {
