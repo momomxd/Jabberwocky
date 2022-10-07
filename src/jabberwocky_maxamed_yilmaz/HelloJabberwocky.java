f6c979783ca926d40e0ba0c11fabd0ede1685896
moham <moham@DESKTOP-OGH83Q8.edu.ds.fhnw.ch>
2022-10-07 10:12:41 +0200

    hello jabberwocky erstellt

---
+++ src/jabberwocky_maxamed_yilmaz/HelloJabberwocky.java
@@ -0,0 +1,11 @@
+package jabberwocky_maxamed_yilmaz;
+
+public class HelloJabberwocky {
+
+	public static void main(String[] args) {
+	System.out.println("Hello Jabberwocky");	//Testklasse
+	System.out.println("hallo Mo");
+
+	}
+
+}
