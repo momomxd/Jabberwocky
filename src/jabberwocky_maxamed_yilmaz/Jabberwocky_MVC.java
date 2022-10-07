b0129d7147d11804c22530eb122564092f500eef
moham <moham@172.28.133.190>
2022-10-07 15:45:22 +0200

    MVC Formatiert

--- src/jabberwocky_maxamed_yilmaz/Jabberwocky_MVC.java
+++ src/jabberwocky_maxamed_yilmaz/Jabberwocky_MVC.java
@@ -8,16 +8,17 @@ public class Jabberwocky_MVC extends Application{
 	private Jabberwocky_Model model;
 	private Jabberwocky_View view;
 
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		model = new Jabberwocky_Model();
 		view = new Jabberwocky_View(primaryStage, model);
 		view.start();
 	}
+	
+	
 	public static void main(String[] args)  {
-
 		launch();
-
 	}
 
 }
