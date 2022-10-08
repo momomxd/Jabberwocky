dccd847a2f473344f5e066c19912c281b1f20cbb
moham <moham@172.28.133.190>
2022-10-08 12:04:09 +0200

    Loop erstellt um Fenstergrösse von 2 Zeichen in Arrays zu speichern

--- src/jabberwocky_maxamed_yilmaz/Jabberwocky_Model.java
+++ src/jabberwocky_maxamed_yilmaz/Jabberwocky_Model.java
@@ -5,20 +5,21 @@ public class Jabberwocky_Model {
 	public static void main(String[] args) {
 		
 	
-		String text ="Für den Aufbau der Statistik verwenden wir ein bewegliches Fenster über die Zeichen des Eingabetexts.";
+		
 	
 	
-	char  char1 = text.charAt(2);
-	System.out.println(char1);
-	String [] textparts = {};
-	
-	for (int i = 0; i<text.length()-1; i++) {								//Loop um über den ganzen text zu gehen
-	textparts [i] = text.substring(text.charAt(i), text.charAt(i+1));		//Substring methode speichert die Fenstergrösse von 2 in einem String Array
+		// Speichert die Zeichen von einer Fenstergrösse von 2 in einem String Array und
+				// verschiebt sich um 1 zeichen
+		String text ="Heute ist ein wunderschöner Tag";				// provisorischer text
+		String[] textparts = new String[text.length()];				// String Array um Zeichen zu speichern
+			
+				for (int count = 0; count < text.length() - 1; count++) {
+					// Loop um über den ganzen text zu gehen
+					textparts[count] = text.charAt(count) + "" + text.charAt(count + 1); 	// Speichert Zeichen in Array
 
-	for(String s : textparts) {												//Ausgabe des Textes
-		System.out.println(s);
+					System.out.println(textparts[count]);									// gibt die Zeichen aus
 
 	}
 	
 
-}}}
+}}
