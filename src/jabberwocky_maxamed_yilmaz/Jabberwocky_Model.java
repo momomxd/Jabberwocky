85ddfa7d3dbc6cda4619d532208ee2e83e98862f
moham <moham@172.28.133.190>
2022-10-08 12:06:49 +0200

    formatiert

--- src/jabberwocky_maxamed_yilmaz/Jabberwocky_Model.java
+++ src/jabberwocky_maxamed_yilmaz/Jabberwocky_Model.java
@@ -8,8 +8,9 @@ public class Jabberwocky_Model {
 		
 	
 	
-		// Speichert die Zeichen von einer Fenstergrösse von 2 in einem String Array und
+				// Speichert die Zeichen von einer Fenstergrösse von 2 in einem String Array und
 				// verschiebt sich um 1 zeichen
+		
 		String text ="Heute ist ein wunderschöner Tag";				// provisorischer text
 		String[] textparts = new String[text.length()];				// String Array um Zeichen zu speichern
 			
