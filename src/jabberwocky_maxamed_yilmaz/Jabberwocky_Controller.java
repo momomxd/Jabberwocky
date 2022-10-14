aead5048f92f2eb140a75ff8fc4e43a613eafb42
moham <moham@DESKTOP-OGH83Q8.edu.ds.fhnw.ch>
2022-10-14 10:20:13 +0200

    Controller erweitert

--- src/jabberwocky_maxamed_yilmaz/Jabberwocky_Controller.java
+++ src/jabberwocky_maxamed_yilmaz/Jabberwocky_Controller.java
@@ -1,5 +1,15 @@
 package jabberwocky_maxamed_yilmaz;
 
 public class Jabberwocky_Controller {
+	
+	Jabberwocky_Model model;
+	Jabberwocky_View view;
+	
+	public Jabberwocky_Controller(Jabberwocky_Model model, Jabberwocky_View view) {
+		
+		this.model = model;
+		this.view = view;
+	}
+
 
 }
