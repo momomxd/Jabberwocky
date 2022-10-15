45d9ceabf0746d47c0aecc100ebad397535db273
muham <muham@DESKTOP-TO30O32.edu.ds.fhnw.ch>
2022-10-15 12:09:38 +0200

    Mögliche nächste Zeichen in eine Liste hinzugefügt.

--- src/jabberwocky_maxamed_yilmaz/TestklasseJabberwocky.java
+++ src/jabberwocky_maxamed_yilmaz/TestklasseJabberwocky.java
@@ -1,5 +1,7 @@
 package jabberwocky_maxamed_yilmaz;
 
+import java.util.ArrayList;
+
 public class TestklasseJabberwocky {
 
 	public static void main(String[] args) {
@@ -17,12 +19,46 @@ public class TestklasseJabberwocky {
 	
 	textStatistik.showList();
 	
+ArrayList <Character> possibleCharacters = new ArrayList<>();	
 	
+	for(int i = 0; i<textStatistik.getTextTeile().size();i++) {
 	
-	
+	String text = textStatistik.getTextTeile().get(i).toString();
+		
+		String [] TextandnextChar = text.split("ⱺ");
+		
+		String nextCharinString = TextandnextChar[1];
+		String window = TextandnextChar[0];
+		char nextChar = nextCharinString.charAt(0);
+		
+		
+		
+		possibleCharacters.add(nextChar);
+				
+		
 	}
+	System.out.println(possibleCharacters);
+ArrayList <String> windowList = new ArrayList<>();	
+	
+	for(int i = 0; i<textStatistik.getTextTeile().size();i++) {
+	
+	String text = textStatistik.getTextTeile().get(i).toString();
+		
+		String [] TextandnextChar = text.split("ⱺ");
+		
+		String nextCharinString = TextandnextChar[1];
+		String window = TextandnextChar[0];
+		char nextChar = nextCharinString.charAt(0);
+	
+		windowList.add(window);
+
+	
+	
+	
+	
+	}	System.out.println(windowList);
 	
 
-	}
+	}}
 
 
--- src/jabberwocky_maxamed_yilmaz/TextStatistik.java
+++ src/jabberwocky_maxamed_yilmaz/TextStatistik.java
@@ -8,6 +8,9 @@ public class TextStatistik {
 	private int Fenstergrösse;
 	private char LastChar = '■';
 	private char splitChar = 'ⱺ';
+	public int getFenstergrösse() {
+		return Fenstergrösse;
+	}
 	private ArrayList TextTeile;
 	
 	
@@ -70,17 +73,50 @@ public class TextStatistik {
 			
 			String [] TextAndNextChar = new String [TextTeile.size()];
 			TextAndNextChar = TextTeile.get(i).toString().split("ⱺ");
-			
-			
-			
-	
+					
 		}
+			
+	}
 	
+//
+
+public ArrayList <Character> possibleCharacter(String input){
 	
+	ArrayList <Character> possibleCharacters = new ArrayList<>();	
+	
+	for(int i = 0; i<TextTeile.size();i++) {
+	
+		input = TextTeile.get(i).toString();
 		
+		String [] TextandnextChar = input.split(Integer.toString(this.splitChar));
+		
+		String nextCharinString = TextandnextChar[1];
+		String window = TextandnextChar[0];
+		char nextChar = nextCharinString.charAt(1);
+		
+		
+		if(input.substring(i, i+Fenstergrösse) == window) {
+		possibleCharacters.add(nextChar);
+				
+		}
+	}
+	System.out.println(possibleCharacters);
+	return possibleCharacters;
+	}
+
+public void generateText() {
+	
+	for(int i = 0; i<TextTeile.size();i++) {
 		
 	}
 	
-
 }
 
+public ArrayList getTextTeile() {
+	return TextTeile;
+}
+
+public void setTextTeile(ArrayList textTeile) {
+	TextTeile = textTeile;
+}
+}
