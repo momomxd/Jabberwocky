b0e653c2786f2fc347fcc5064cc860db79a2e557
muham <muham@DESKTOP-TO30O32.edu.ds.fhnw.ch>
2022-10-15 12:36:33 +0200

    Formatiert: Zeilen gelöscht oder verschoben für die bessere Übersicht

--- src/jabberwocky_maxamed_yilmaz/TextStatistik.java
+++ src/jabberwocky_maxamed_yilmaz/TextStatistik.java
@@ -9,27 +9,23 @@ public class TextStatistik {
 	private char LastChar = '■';
 	private char splitChar = 'ⱺ';
 	private ArrayList TextTeile;
-	
-	
-	
+		
 	
 	public TextStatistik(int Fenstergrösse, String inputText) {
 		this.Fenstergrösse = Fenstergrösse;
-		this.inputText = inputText;
-		
+		this.inputText = inputText;	
 		TextTeile = new ArrayList<String>();
-		
+	
 		GenerateTextTeilListe();
 		
 	}
-	boolean endeListe = false;
 	
 	/*
 	 * Die Methode GenerateTextTeilListe speichert die Fenstergrösse und das nächste Zeichen in Strings
 	 * und speichert diese in der ArrayListe "TextTeile"
 	 */
 	public void GenerateTextTeilListe() {
-		
+		boolean endeListe = false;
 		int index = 0;
 		
 		while (!endeListe) {
@@ -48,10 +44,8 @@ public class TextStatistik {
 		// If schleife kontrolliert ob wir das letzte Zeichen '■' erreicht wurde
 		 if(nextChar == this.LastChar) {
 		 	endeListe = true;
-		 }
-			 
-		}
-		
+		 }	 
+		}	
 	}
 	
 	// Methode um Liste zu überprüfen
@@ -63,61 +57,51 @@ public class TextStatistik {
 	}
 	
 	// Methode um nächstes Zeichen zu finden  
-	
 	public void findPossibleChar() {
 		
 		for(int i = 0; i > this.TextTeile.size(); i++) {
 			
 			String [] TextAndNextChar = new String [TextTeile.size()];
-			TextAndNextChar = TextTeile.get(i).toString().split("ⱺ");
-					
-		}
-			
+			TextAndNextChar = TextTeile.get(i).toString().split("ⱺ");		
+		}		
 	}
 	
-//
-
-public ArrayList <Character> possibleCharacter(String input){
+	// Methode gibt eine ArrayListe mit allen möglichen nächsten Zeichen zurück
+	public ArrayList <Character> possibleCharacter(String input){
 	
 	ArrayList <Character> possibleCharacters = new ArrayList<>();	
 	
 	for(int i = 0; i<TextTeile.size();i++) {
-	
 		input = TextTeile.get(i).toString();
 		
 		String [] TextandnextChar = input.split(Integer.toString(this.splitChar));
-		
 		String nextCharinString = TextandnextChar[1];
 		String window = TextandnextChar[0];
 		char nextChar = nextCharinString.charAt(1);
 		
 		
 		if(input.substring(i, i+Fenstergrösse) == window) {
-		possibleCharacters.add(nextChar);
-				
+		possibleCharacters.add(nextChar);			
+			}
+		}
+		return possibleCharacters;
+	}
+	
+	// Methode um einen neuen Text zu generieren
+	public void generateText() {
+	for(int i = 0; i<TextTeile.size();i++) {
+		}	
+	}
+
+	public ArrayList getTextTeile() {
+	return TextTeile;
+	}
+
+	public void setTextTeile(ArrayList textTeile) {
+	TextTeile = textTeile;
+	}
+
+	public int getFenstergrösse() {
+	return Fenstergrösse;
 		}
 	}
-	System.out.println(possibleCharacters);
-	return possibleCharacters;
-	}
-
-public void generateText() {
-	
-	for(int i = 0; i<TextTeile.size();i++) {
-		
-	}
-	
-}
-
-public ArrayList getTextTeile() {
-	return TextTeile;
-}
-
-public void setTextTeile(ArrayList textTeile) {
-	TextTeile = textTeile;
-}
-
-public int getFenstergrösse() {
-	return Fenstergrösse;
-}
-}
