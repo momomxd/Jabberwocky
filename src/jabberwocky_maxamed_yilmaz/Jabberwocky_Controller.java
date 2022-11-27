049de3766e47fe5009be77bf29250cea49a348db
moham <moham@172.28.136.218>
2022-11-27 10:00:56 +0100

    formatiert

--- src/jabberwocky_maxamed_yilmaz/Jabberwocky_Controller.java
+++ src/jabberwocky_maxamed_yilmaz/Jabberwocky_Controller.java
@@ -6,23 +6,18 @@ import javafx.application.*;
 import javafx.stage.*;
 
 public class Jabberwocky_Controller {
-	
+
 	Jabberwocky_Model model;
 	Jabberwocky_View view;
-	
+
 	public Jabberwocky_Controller(Jabberwocky_Model model, Jabberwocky_View view) {
-		
+
 		this.model = model;
 		this.view = view;
-			
-		
+
 		view.btnGenerate.setOnAction((event) -> {
 			model.generateText();
 		});
 	}
-
-
-
-
 
 }
